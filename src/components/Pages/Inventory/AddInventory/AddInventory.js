666f7380889daf1774e3cebd8fc594cf80463fde
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-05-08 01:10:48 +0600

    add item - post method added

--- src/components/Pages/Inventory/AddInventory/AddInventory.js
+++ src/components/Pages/Inventory/AddInventory/AddInventory.js
@@ -2,7 +2,22 @@ import React from "react";
 import { useForm } from "react-hook-form";
 const AddInventory = () => {
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data, e) => {
+    console.log(data);
+    const url = `http://localhost:5000/product`;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        e.target.reset();
+      });
+  };
   return (
     <section className="addInventory">
       <div className="container">
