257a4dcf1884d2416d5aaa6dcc6ae42c67e2b849
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-07-08 00:43:23 +0600

    banner image changed

--- src/components/Pages/Home/Banner/Banner.js
+++ src/components/Pages/Home/Banner/Banner.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Banner.css";
-import banner1 from "../../../../images/banner.webp";
 import bannerRight from "../../../../images/banner-right-1.webp";
 import bannerRight2 from "../../../../images/banner-right-2.webp";
+import bannerMain from "../../../../images/banner1.jpg";
 import { useNavigate } from "react-router-dom";
 const Banner = () => {
   const navigate = useNavigate();
@@ -12,8 +12,12 @@ const Banner = () => {
         <div className="row">
           <div className="col-lg-8 banner-main mt-3">
             <div className="hover-block"></div>
-            <div className="img-wrapper">
-              <img className="img-fluid" src={banner1} alt="banner main img" />
+            <div className="img-wrapper d-block w-100">
+              <img
+                className="img-fluid w-100 d-block"
+                src={bannerMain}
+                alt="banner main img"
+              />
             </div>
             <div className="banner-content">
               <p className="lead mb-0">Top notch gadgets</p>
